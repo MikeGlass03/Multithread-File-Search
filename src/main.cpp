f12b6d4c9950a03d61ab03d1f627ada771578283
MikeGlass03 <166069936+MikeGlass03@users.noreply.github.com>
2026-09-16 12:29:47 -0700

    Directory and Drive Search and C++ Env Fix

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,34 @@
 
 int main() {
     std::string file_name;
-    std::cout << "Input the name of the file to search for: \n";
-    std::cin >> file_name;
-    
-    auto matches = search_files(".", file_name);
+    std::string directory;
 
-    if(matches.empty()) {
-        std::cout << "Could not find the file you entered.";
-        return 0;
+    std::cout << "Input the drive or directory to begin search (for the whole drive, use C:\\ or D:\\): ";
+    std::cin >> directory;
+
+    if (directory.empty()) {
+        directory = "C:\\";
     }
 
-    for(const auto& p : matches) {
-        std::cout << p << "\n"; 
+    std::cout << "Input the name of the file to search for: ";
+    std::cin >> file_name;
+
+    std::filesystem::path root_path = directory;
+
+    if (!std::filesystem::exists(root_path) || !std::filesystem::is_directory(root_path)) {
+        std::cout << "The directory does not exist.\n";
+        return 1;
+    }
+
+    auto matches = search_files(root_path, file_name);
+
+    if (matches.empty()) {
+        std::cout << "Could not find the file you entered in " << root_path << "\n";
+        return 1;
+    }
+
+    for (const auto& p : matches) {
+        std::cout << p << "\n";
     }
 
     return 0;
